bcb2e4c83f9b092a5d708562a38a9dd20857e316
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2025-05-24 00:49:00 +0530

    Query Change

--- mysql/queries.sql
+++ mysql/queries.sql
@@ -1,11 +1,13 @@
 -- Query 1
 SET @user_id = 1;
-SET @current_date = '2025-05-25 00:00:00';
 
 SELECT * 
 FROM Events 
 WHERE city = (SELECT city FROM Users WHERE Users.user_id = @user_id) 
-    AND start_date > @current_date 
+    AND status = 'upcoming'
+    AND event_id IN
+    (SELECT event_id FROM Registration
+    WHERE user_id = @user_id)
 ORDER BY start_date ASC;
 
 -- Query 2
